85ff3a4f37b54ff5132955a7ec178f6c8e6de4cb
Richard Venneman <richardvenneman@me.com>
2019-08-19 13:42:01 +0100

    Implement layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,9 @@
-<script>
-	export let name;
-</script>
-
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
-
-<div class="bg-black text-white">test</div>
+<div class="flex flex-wrap md:h-screen">
+	<div class="p-5 bg-white w-full order-2 flex-none md:flex-1 md:p-5">
+		Content
+	</div>
+	<aside class="px-10 py-5 mb-4 w-full text-center order-1 md:mb-0 md:max-w-sm md:text-left">
+		<h1 class="text-3xl leading-none text-gray-900">Rails server config</h1>
+		<h2 class="text-1xl text-gray-600 mt-2">Configure your Ruby web server for maximum performance</h2>
+	</aside>
+</div>
